ac7ad1f2e81b4bc6e4341a0007382c7bd584a5c8
peter macharia <mainapeter081@gmail.com>
2018-11-27 08:57:12 +0300

    create a header file for common libraries

---
+++ netheaders.h
@@ -0,0 +1,14 @@
+#ifndef NETWORK_H
+#define NETWORK_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+#endif
